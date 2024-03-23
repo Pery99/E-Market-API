60dcaa6dae411a61e843635f850d59cfb4540fc9
Pery99 <101347317+Pery99@users.noreply.github.com>
2024-03-23 12:28:36 +0100

    Added jwt

--- controller/userController.auth.js
+++ controller/userController.auth.js
@@ -1,16 +1,21 @@
 const User = require("../model/user.model");
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
 
 const login = async (req, res) => {
     try {
         const { email, password } = req.body
         const validUser = await User.findOne({ email })
         if (!validUser) return res.status(401).json({ msg: 'Wrong Credentials' })
-        const isMatch = bcrypt.compareSync(password, validUser.password)
+        const isMatch = await bcrypt.compareSync(password, validUser.password)
         if (!isMatch) return res.status(401).json({ msg: 'Wrong Credentials' })
-        res.status(200).json({ msg: 'Logged in' })
+        const token = await jwt.sign({ id: validUser._id }, process.env.JWT_KEY)
+        res.cookie('token', token, {
+            httpOnly: true
+        })
+        res.status(200).json({ email: validUser.email })
     } catch (error) {
-
+        console.log(error);
     }
 }
 const register = async (req, res) => {
